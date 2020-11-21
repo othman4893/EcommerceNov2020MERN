17df7d382f1d794db5f3cfa812fefcce0c4a5a30
othman4893 <othman4893@gmail.com>
2020-11-21 18:47:05 +0100

    React frontend..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
-    <>
-      {" "}
-      <h1> Welcome to E commerce</h1>
-    </>
+    <Router>
+      <main>
+        <Header />
+        <Container className="py-3">
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+
+      <Footer />
+    </Router>
   );
 };
 
